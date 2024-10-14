791610b44bee2d517fb9d0ed03a323ec8c0ec17c
jishrami <jramirez@byui.edu>
2024-10-14 15:53:26 -0600

    feat: small frontend changes
    added routes to public, images to populate, and started work on front page

--- routes/static.js
+++ routes/static.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.use(express.static('public'));
-router.use("/images", express.static(__dirname + "public/images"));
-router.use("/scripts", express.static(__dirname + "public/scripts"));
-router.use("/styles", express.static(__dirname + "public/styles"));
+router.use("/images", express.static(__dirname + "/public/images"));
+router.use("/scripts", express.static(__dirname + "/public/scripts"));
+router.use("/styles", express.static(__dirname + "/public/styles"));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,14 +6,15 @@ const expressLayout = require('express-ejs-layouts'); // Import express layouts
 
 // Define port and host
 const port = process.env.PORT || 3000;
-const host = process.env.HOST;
+const host = process.env.HOST || 'localhost';
 
 app.set('view engine', 'ejs'); // Set view engine to ejs
 app.use(expressLayout); // Use express layouts
 app.set('layout', './layouts/layout'); // Set layout to layout.ejs
-app.use(express.static('static')); // Use express static    
 
-app.get('/',  static); // Index route
+//Routes
+app.use(static) // Static route
+    .get('/', (req, res) => res.render('index', { title: 'Home' })); // Index route
 
 app.listen(port, host, () => {
     console.log(`Server running on http://${host}:${port}`);
